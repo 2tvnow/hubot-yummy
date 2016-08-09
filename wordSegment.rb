d178919206d28142c2ff4443a95f89b803e9ed0a
may <happycat6323@yahoo.com.tw>
2016-08-09 11:00:00 +0800

    Fix bug

--- wordSegment.rb
+++ wordSegment.rb
@@ -1,5 +1,7 @@
+# encoding: utf-8
+
 require './bin/ckip_client-0.0.7/lib/CKIP_Client.rb'
 
-text = CKIP.segment("然後他問奧運對他們來說是非常重要的事長官會狂盯")
+text = CKIP.segment("雖然我也會小課一下  but 上次聽他說那個啥抽生肖的  發覺完全等級不同啊ㄟ   大家決定一下明天中餐吃啥吧完美的狀況 >  空間夠大   可以做一段公車(抓一下) 拉麵店優先!星星你的任務")
 
 puts text
