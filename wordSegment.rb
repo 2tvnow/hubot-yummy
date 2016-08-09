396d41c78933b4add7c10fa8d3c52c3c08e6c6e9
may <happycat6323@yahoo.com.tw>
2016-08-09 17:57:35 +0800

    test

--- wordSegment.rb
+++ wordSegment.rb
@@ -2,6 +2,10 @@
 
 require './bin/ckip_client-0.0.7/lib/CKIP_Client.rb'
 
-text = ARGV.first.encode("utf-8")
+text = ARGV.first.encode("utf-8", {
+                                  :invalid => :replace,
+                                  :undef   => :replace,
+                                  :replace => '?'
+                                })
 
-puts "#{text}"
+puts CKIP.segment(text)
